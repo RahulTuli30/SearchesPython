98a74433c0c8bdd1365f791173335c0b0911bf80
RahulTuli30 <rt3991@g.rit.edu>
2019-10-13 12:06:01 -0700

    Initial BFS Complete

--- BFS.py
+++ BFS.py
@@ -1,3 +1,37 @@
 import sys
+from collections import defaultdict
 
-def BFS(initial_state):
+
+def BFS(initial_state, isGoal, successors):
+    assert initial_state, "Provide a Valid Initial State"
+    assert isGoal, "Provide a Valid isGoal function"
+    assert successors, "Provide a Valid Successor function"
+
+    Que = [initial_state]
+    parents = defaultdict(lambda: None)
+
+    done = False
+
+    while not done:
+        state = Que.pop()
+
+        if isGoal(state):
+            print("Goal Found! Building Path")
+            return build_path(state, parents)
+
+        for child in successors(state):
+            if not parents[child]:
+                parents[child] = state
+                Que.append(child)
+
+        done = len(Que) == 0
+
+def build_path(state, parents):
+    path  = [state]
+
+    while parents[state]:
+        path.append(parents[state])
+        state = parents[state]
+
+    print("Built path! Now returning it")
+    return reversed(path)
