201e5baf1105738184b1eed98230c6d0bc7556dd
RahulTuli30 <rt3991@g.rit.edu>
2019-10-13 12:44:33 -0700

    Added a Iteration limit to BFS

--- BFS.py
+++ BFS.py
@@ -3,25 +3,27 @@ from collections import defaultdict
 
 
 class BFS:
-    __slots__ = "initial_state", "goal_state", "successors"
+    __slots__ = "initial_state", "goal_state", "successors", "ITERATION_LIMIT"
 
-    def __init__(self, initial_state, goal_state, successors):
-        self.check_validity(initial_state, goal_state, successors)
+    def __init__(self, initial_state, goal_state, successors, iter_limit=1000):
+        self.check_validity(initial_state, goal_state, successors, iter_limit)
         self.initial_state, self.goal_state, self.successors = initial_state, goal_state, successors
+        self.ITERATION_LIMIT = iter_limit
 
     @staticmethod
-    def check_validity(initial_state, goal_state, successors):
+    def check_validity(initial_state, goal_state, successors, iter_limit):
         assert initial_state, "Provide Initial State"
         assert goal_state, "Provide Valid Goal State"
         assert successors, "Provide a Valid Successor function"
         assert callable(successors), "Successors has to be a callable function"
+        assert type(iter_limit) == int, "INVALID ITERATION LIMIT"
 
     def start(self):
         Que = [self.initial_state]
         parents = defaultdict(lambda: None)
 
         done = False
-
+        i = 0
         while not done:
             state = Que.pop()
 
@@ -33,8 +35,10 @@ class BFS:
                 if not parents[child]:
                     parents[child] = state
                     Que.append(child)
+            i += 1
+            done = len(Que) == 0 or i > self.ITERATION_LIMIT
 
-            done = len(Que) == 0
+        print("Solution NOT found, or Iteration Limit reached")
 
     def isGoal(self, state):
         return state == self.goal_state
